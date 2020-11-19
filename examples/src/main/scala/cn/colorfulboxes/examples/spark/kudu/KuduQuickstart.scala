d653c65baddf8aa2304c0a77d18a166920fe39b7
d0ma4t2 <SsXxMm4270>
2020-11-19 18:13:23 +0800

    add kudu

--- examples/src/main/scala/cn/colorfulboxes/examples/spark/kudu/KuduQuickstart.scala
+++ examples/src/main/scala/cn/colorfulboxes/examples/spark/kudu/KuduQuickstart.scala
@@ -5,8 +5,10 @@ import cn.colorfulboxes.utils.spark.config.DBconfig
 import org.apache.kudu.client.CreateTableOptions
 import org.apache.kudu.spark.kudu.KuduContext
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructType}
+import org.junit.jupiter.api.{AfterEach, BeforeEach, Test}
 
 import scala.collection.JavaConverters._
 
@@ -14,25 +16,66 @@ import scala.collection.JavaConverters._
 /**
  * https://kudu.apache.org/docs/developing.html#_kudu_integration_with_spark
  */
-object KuduQuickstart {
+class KuduQuickstart {
 
-  // todo no test
-  def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkUtil.getSpark(this.getClass.getSimpleName)
 
-    val sc: SparkContext = spark.sparkContext
-    val kuduContext = new KuduContext(DBconfig.getInstance.getKudu.url, sc)
+  var kuduContext: KuduContext = _
+  var spark: SparkSession = _
+  var sc: SparkContext = _
+
+  @BeforeEach
+  def createContext() = {
+    spark = SparkUtil.getSpark(this.getClass.getSimpleName)
+    sc = spark.sparkContext
+    kuduContext = new KuduContext(DBconfig.getInstance.getKudu.url, sc)
+  }
+
+  @Test
+  def createTable(): Unit = {
+    // impala表名
+    // println(kuduContext.tableExists("impala::POC_TEST.sdc"))
 
     // Delete the table if it already exists.
-    if(kuduContext.tableExists("tableName")) {
-      kuduContext.deleteTable("tableName")
+    if (kuduContext.tableExists("house_kudu.people")) {
+      kuduContext.deleteTable("house_kudu.people")
     }
 
-    kuduContext.createTable("tableName", new StructType(), Seq("",""),
-      new CreateTableOptions()
-        .setNumReplicas(3)
-        .addHashPartitions(List("").asJava, 4))
+    val tableSchema: StructType = new StructType()
+      .add("name", StringType, nullable = false)
+      .add("age", IntegerType, nullable = false)
+      .add("gpa", DoubleType, nullable = false)
 
+    val keys = Seq("name")
+
+    val options = new CreateTableOptions()
+      .setNumReplicas(7)
+      .addHashPartitions(List("name").asJava, 4)
+
+    kuduContext.createTable("house_kudu.people", tableSchema, keys, options)
+  }
+
+  @Test
+  def upsert() = {
+    val peoples = for (i <- Range(0, 1000)) yield People(s"name$i", i % 50, i * 100)
+    val peoplesRDD: RDD[People] = sc.parallelize(peoples)
+    val peoplesDF = spark.createDataFrame(peoplesRDD)
+    kuduContext.upsertRows(peoplesDF, "house_kudu.people")
+  }
+
+  @Test
+  def delete() = {
+    val peoples = for (i <- Range(0, 1000)) yield People(s"name$i", i % 50, i * 100)
+    val peoplesRDD: RDD[People] = sc.parallelize(peoples)
+    val peoplesDF = spark.createDataFrame(peoplesRDD)
+    kuduContext.deleteRows(peoplesDF.select("name"), "house_kudu.people")
 
   }
+
+  @AfterEach
+  def closeContext() = {
+    sc.stop()
+    spark.stop()
+  }
 }
+
+case class People(name: String, age: Int, gpa: Double)
---
+++ examples/src/main/scala/cn/colorfulboxes/examples/spark/kudu/KuduSparkDF.scala
@@ -0,0 +1,54 @@
+package cn.colorfulboxes.examples.spark.kudu
+
+import cn.colorfulboxes.utils.spark.SparkUtil
+import cn.colorfulboxes.utils.spark.config.DBconfig
+import org.apache.kudu.spark.kudu.KuduContext
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.junit.jupiter.api.{AfterEach, BeforeEach, Test}
+
+class KuduSparkDF {
+
+  var kuduContext: KuduContext = _
+  var spark: SparkSession = _
+  var sc: SparkContext = _
+
+  @BeforeEach
+  def createContext() = {
+    spark = SparkUtil.getSpark(this.getClass.getSimpleName)
+    sc = spark.sparkContext
+    kuduContext = new KuduContext(DBconfig.getInstance.getKudu.url, sc)
+  }
+
+  @Test
+  def write() = {
+    val peoples = for (i <- Range(1000, 2000)) yield People(s"name$i", i % 50, i * 100)
+    val peoplesRDD: RDD[People] = sc.parallelize(peoples)
+
+    spark.createDataFrame(peoplesRDD)
+      .write
+      .option("kudu.table","house_kudu.people")
+      .option("kudu.master",DBconfig.getInstance.getKudu.url)
+      .mode("append")
+      .format("kudu")
+      .save()
+  }
+
+  @Test
+  def read() = {
+
+    spark.read
+      .option("kudu.table","house_kudu.people")
+      .option("kudu.master",DBconfig.getInstance.getKudu.url)
+      .format("kudu")
+      .load
+      .show(10)
+  }
+
+  @AfterEach
+  def closeContext() = {
+    sc.stop()
+    spark.stop()
+  }
+}
